8b2bb5216089951038208da97648ce9b420bc74a
AsuraA93 <56329129+JamesAsuraA93@users.noreply.github.com>
2021-08-18 11:24:46 +0700

    maim.cpp
    for bua naka CMU

---
+++ main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  BUA_SwitchCase
+//
+//  Created by Peerapat Supasri on 18/8/2564 BE.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "น้อนบัว :";
+    cin >> x;
+    int arr[] = {x};
+    switch (arr[0]){
+    case 1 ... 10:
+        cout << "Start" << endl;
+        break;
+    case 11 ... 19:
+        cout << "Mid" << endl;
+        break;
+    case 20 ... 31:
+        cout << "End" << endl;
+        break;
+    default:
+        cout << "InvalidNumber" << endl;
+        break;
+    }
+
+return 0;
+
+}
