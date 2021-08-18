f576b60e794513c647b899df5bca89d9561295f9
AsuraA93 <56329129+JamesAsuraA93@users.noreply.github.com>
2021-08-18 11:37:44 +0700

    Edit some switch arg

--- main.cpp
+++ main.cpp
@@ -13,8 +13,7 @@ int main()
     int x;
     cout << "น้อนบัว :";
     cin >> x;
-    int arr[] = {x};
-    switch (arr[0]){
+    switch (x){
     case 1 ... 10:
         cout << "Start" << endl;
         break;
@@ -32,3 +31,33 @@ int main()
 return 0;
 
 }
+
+// or 
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int x;
+//     cout << "น้อนบัว :";
+//     cin >> x;
+//     int arr[] = {x};
+//     switch (arr[0]){
+//     case 1 ... 10:
+//         cout << "Start" << endl;
+//         break;
+//     case 11 ... 19:
+//         cout << "Mid" << endl;
+//         break;
+//     case 20 ... 31:
+//         cout << "End" << endl;
+//         break;
+//     default:
+//         cout << "InvalidNumber" << endl;
+//         break;
+//     }
+
+// return 0;
+
+// }
